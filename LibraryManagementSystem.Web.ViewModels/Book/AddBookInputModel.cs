7541b18d0c97c8d10a3de0fa3cc6330e45cfb1b0
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2024-02-16 21:47:47 +0200

    AddBookInputModel - the first ViewModel

--- LibraryManagementSystem.Web.ViewModels/Book/AddBookInputModel.cs
+++ LibraryManagementSystem.Web.ViewModels/Book/AddBookInputModel.cs
@@ -5,58 +5,37 @@ namespace LibraryManagementSystem.Web.ViewModels.Book
 {
     public class AddBookInputModel
     {
-        [RegularExpression(ISBNRegexPattern)]
+        [RegularExpression(ISBNRegexPattern, ErrorMessage = "ISBN must follow this pattern")]
         [Display(Name = "International Standard Book Number")]
+        [StringLength(ISBNMaxLength, ErrorMessage = "ISBN number must be max 22 characters long.")]
         public string? ISBN { get; set; }
 
         [Required]
-        [EmailAddress]
-        public string Email { get; set; } = null!;
+        [Display(Name = "Title of the Book")]
+        [StringLength(TitleMaxLength, ErrorMessage = "Title must be between 2 and 22 characters long.",
+        MinimumLength = TitleMinLength)]
+        public string Title { get; set; } = null!;
 
-        [Phone]
-        [RegularExpression("\\+[0-9]{1,3}-[0-9]{1,10}", ErrorMessage = "Your phone number must follow the pattern above")]
-        [StringLength(ValidationalConstants.GymConstants.PhoneNumberMaxLength, ErrorMessage = "The phone number must be at least 7 and at max 15 characters long.",
-         MinimumLength = ValidationalConstants.GymConstants.PhoneNumberMinLength)]
-        [Display(Name = "Phone number")]
-        public string PhoneNumber { get; set; } = null!;
-
-        [Required]
-        [Display(Name = "Logo")]
-        public IFormFile LogoFile { get; set; } = null!;
-
-        [Display(Name = "Gallery")]
-        public IEnumerable<IFormFile> GalleryImagesFiles { get; set; }
-
-        public ImageUploadResult? LogoResultParams { get; set; }
-
-        public ICollection<ImageUploadResult> GalleryImagesResultParams { get; set; }
+        [Display(Name = "The year of book publish")]
+        [DisplayFormat(DataFormatString = DateFormat)]
+        public DateOnly? YearPublished { get; set; }
 
         [Required]
-        [Display(Name = "Webiste Url")]
-        public string WebsiteUrl { get; set; } = null!;
-
-        [Required]
+        [Display(Name = "Description of the book")]
+        [StringLength(DescriptionMaxLength, ErrorMessage = "Description must be between 50 and 1500 characters long.",
+        MinimumLength = DescriptionMinLength)]
         public string Description { get; set; } = null!;
 
-        [Required]
-        public string GymType { get; set; } = null!;
+        [Display(Name = "Publisher of the book")]
+        [StringLength(PublisherMaxLength, ErrorMessage = "Publisher name must be between 5 and 100 characters long.",
+        MinimumLength = PublisherMinLength)]
+        public string? Publisher { get; set; } = null!;
 
-        [Display(Name = "Gym Types")]
-        public IEnumerable<string> GymTypes { get; set; }
+        [Display(Name = "Cover image of the book")]
+        [StringLength(CoverImagePathUrlMaxLength)]
+        public string? CoverImagePathUrl { get; set; } = null!;
 
-        [Required]
-        public string Address { get; set; } = null!;
-
-        [Required]
-        public string CountryId { get; set; } = null!;
-
-        [Display(Name = "Countries")]
-        public IEnumerable<SelectListItem> CountriesSelectList { get; set; }
-
-        [Required]
-        public string TownId { get; set; } = null!;
-
-        [Display(Name = "Towns")]
-        public IEnumerable<SelectListItem> TownsSelectList { get; set; }
+        //[Required]
+        //public Guid AuthorId { get; set; }
     }
 }
