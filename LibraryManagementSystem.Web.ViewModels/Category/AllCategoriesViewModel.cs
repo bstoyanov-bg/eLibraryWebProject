1f7801b05457a782b490ece5e811421fda7d5414
Bozhidar Stoyanov <stoyanov.bojidar@gmail.com>
2024-04-01 16:55:00 +0300

    Formating Views.

--- LibraryManagementSystem.Web.ViewModels/Category/AllCategoriesViewModel.cs
+++ LibraryManagementSystem.Web.ViewModels/Category/AllCategoriesViewModel.cs
@@ -1,7 +1,10 @@
-﻿namespace LibraryManagementSystem.Web.ViewModels.Category
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LibraryManagementSystem.Web.ViewModels.Category
 {
     public class AllCategoriesViewModel
     {
+        [Required(ErrorMessage = "Please select at least one author.")]
         public int Id { get; set; }
 
         public string Name { get; set; } = null!;
